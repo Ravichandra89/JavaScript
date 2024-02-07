db48b90de3d6da720afd0d12d0f1882dddf04a10
Ravi chandra <134200599+Ravichandra89@users.noreply.github.com>
2024-02-07 13:44:47 +0000

    SwitchCase

---
+++ JavaScript/ControlFlow/Switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 4
+
+switch(month){
+    case 1 :
+        console.log('Jan');
+        break;
+    case 2 :
+        console.log('Feb');
+        break;
+    case 3 :
+        console.log('March');
+        break;
+    case 4 :
+        console.log('Aprail');
+        break;
+    default :
+        console.log('Default case match');
+        break;
+}
