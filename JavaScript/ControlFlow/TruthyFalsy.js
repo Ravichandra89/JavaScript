8764b5dcadd0c008125d9adc3011145e5ec73071
Ravi chandra <134200599+Ravichandra89@users.noreply.github.com>
2024-02-07 13:50:41 +0000

    truthy

--- JavaScript/ControlFlow/TruthyFalsy.js
+++ JavaScript/ControlFlow/TruthyFalsy.js
@@ -1,7 +1,30 @@
-const userEmail = "hello@gmail.com"
+const userEmail = ""
 
 if(userEmail){
     console.log('Got user email');
 }else{
-    console.log()
+    console.log("Don't Have user email")
 }
+
+// falsy
+false , 0 ,-0,BigInt,0n,"",null,undefined,NaN
+
+// Truthy
+// "0","false" {} [] " "
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length == 0){
+    console.log('Object is empty');
+}
+
+
+//  To check array empty or not we uses the length function
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1
+val1 = null ?? 10
+val1 = undefined ?? 3
+
+console.log(val1);
